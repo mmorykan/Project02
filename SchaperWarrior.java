4f715b8d7bb34c30741b0ea8c09aa887d40225a4
morykanm <morykanm@moravian.edu>
2021-03-23 11:01:13 -0400

    Documented Schaper classes

--- SchaperWarrior.java
+++ SchaperWarrior.java
@@ -1,17 +1,36 @@
+/**
+ * Warrior Strategy
+ * Author: Dr. Schaper
+ * Modified by Mark Morykan
+ */
 package Project02;
 
 import Project02.People;
 import Project02.PeopleType;
 
-
+/**
+ * Warrior Strategy
+ */
 public class SchaperWarrior extends People
 {
+
+    /**
+     * Call base class constructor with warrior description
+     * @param nation Nation of warrior
+     * @param tribe Tribe of warrior
+     * @param lifePoints Amount of life points
+     */
     public SchaperWarrior(String nation, String tribe, int lifePoints)
     {
         super(nation, tribe, PeopleType.warrior, lifePoints);
         myDescription = "\tSchaper Warrior";
     }
 
+    /**
+     * Use all opponents life points as damage if the difference in health is greater than 0. Else, use own points
+     * @param otherPerson Encounters person
+     * @return Damage points to use
+     */
     public int encounterUgly(People otherPerson) {
         int lifePoints;
         if (this.getLifePoints() - otherPerson.getLifePoints() > 0)
@@ -25,6 +44,11 @@ public class SchaperWarrior extends People
         return lifePoints;
     }
 
+    /**
+     * Heal during friendly encounters
+     * @param otherPerson Encountered character
+     * @return The amount of damage points to use
+     */
     public int encounterFriendly(People otherPerson) {
         int lifePoints = 0;
         if(otherPerson.getLifePoints() < this.getLifePoints())
--- SchaperWizard.java
+++ SchaperWizard.java
@@ -1,17 +1,37 @@
+/**
+ * Wizard Strategy
+ * Author: Dr. Strategy
+ * Modified by Mark Morykan
+ */
 package Project02;
 
 import Project02.People;
 import Project02.PeopleType;
 
-
+/**
+ * Wizard Strategy
+ */
 public class SchaperWizard extends Project02.People
 {
+
+    /**
+     * Call base class constructor with wizard description
+     * @param nation The wizard's nation
+     * @param tribe The wizard's tribe
+     * @param lifePoints Amount of health points
+     */
     public SchaperWizard(String nation, String tribe, int lifePoints)
     {
         super(nation, tribe, PeopleType.wizard, lifePoints);
         myDescription = "\tSchaper Wizard";
     }
 
+    /**
+     * Run away from encountered person if their life points are less and they are warrior.
+     * Attack a wizard
+     * @param otherPerson Encountered person
+     * @return Amount of damage points to use
+     */
     public int encounterUgly(People otherPerson) {
         int lifePoints = 0;
         if (otherPerson.getLifePoints() < this.getLifePoints())
@@ -28,6 +48,11 @@ public class SchaperWizard extends Project02.People
         return lifePoints;
     }
 
+    /**
+     * Heal friendly player
+     * @param otherPerson Encountered player
+     * @return Amount of points to heal friendly player with
+     */
     public int encounterFriendly(People otherPerson) {
         int lifePoints = 0;
         if (otherPerson.getLifePoints() < this.getLifePoints()) // heal a friend
